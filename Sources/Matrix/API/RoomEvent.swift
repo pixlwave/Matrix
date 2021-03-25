1cf64465408ea9493e732375d6c21d94db37c5b5
Doug <git@pixlwave.uk>
2021-03-25 11:40:44 +0000

    Add support for Redactions.

--- Sources/Matrix/API/RoomEvent.swift
+++ Sources/Matrix/API/RoomEvent.swift
@@ -8,23 +8,33 @@ public struct RoomEvent: Codable {
     public let date: Date
 //    public let unsigned: UnsignedData
     
+    // m.room.redaction
+    public let redacts: String?
+    
     enum CodingKeys: String, CodingKey {
         case content
         case type
         case eventID = "event_id"
         case sender
         case date = "origin_server_ts"
+        case redacts
     }
     
     public struct RoomEventContent: Codable {
         public let body: String?
         public let relationship: Relationship?
+        
+        // edit event
         public let newContent: NewContent?
+        
+        // m.room.redaction
+        public let reason: String?
         
         enum CodingKeys: String, CodingKey {
             case body
             case relationship = "m.relates_to"
             case newContent = "m.new_content"
+            case reason
         }
         
         public struct NewContent: Codable {
