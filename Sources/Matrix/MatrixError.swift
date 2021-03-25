a8ccadc71f926d9ee2acbcf9e4d332bccf7c535a
Doug <git@pixlwave.uk>
2021-03-25 12:24:22 +0000

    Consolidate initialSync and longPoll. Pass the error message up when setting a .syncError.

--- Sources/Matrix/MatrixError.swift
+++ Sources/Matrix/MatrixError.swift
@@ -5,4 +5,17 @@ public enum MatrixError: Error {
     case decodeError(error: DecodingError)
     case errorResponse(error: ErrorResponse)
     case unknown(error: Error)
+    
+    public var description: String {
+        switch self {
+        case .urlError(let error):
+            return error.localizedDescription
+        case .decodeError(let error):
+            return error.localizedDescription
+        case .errorResponse(let error):
+            return error.message
+        case .unknown(let error):
+            return error.localizedDescription
+        }
+    }
 }
